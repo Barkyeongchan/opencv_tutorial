2dd552162d5f2086a2b4d7bc4d6b032ca9d8e383
Barkyeongchan <xodudcksxo@gmail.com>
2025-07-31 10:27:59 +0900

    이미지 회전 함수 추가

--- 04_opencv/src/rotate_martix.py
+++ 04_opencv/src/rotate_martix.py
@@ -20,10 +20,22 @@ m90 = np.float32( [[ np.cos(d90), -1* np.sin(d90), rows],
 r45 = cv2.warpAffine(img,m45,(cols,rows))
 r90 = cv2.warpAffine(img,m90,(rows,cols))
 
-# 2이미지 출력
+# @cv2.getRotationMatrix2D() 함수 사용해 회전
+# 회전축:중앙, 각도:45, 배율:0.5
+func_m45 = cv2.getRotationMatrix2D((cols/2,rows/2),45,0.5) 
+# 회전축:중앙, 각도:90, 배율:1.5
+func_m90 = cv2.getRotationMatrix2D((cols/2,rows/2),90,1.5)
+
+# @변환 행렬 적용
+img45 = cv2.warpAffine(img, func_m45,(cols, rows))
+img90 = cv2.warpAffine(img, func_m90,(cols, rows))
+
+# @이미지 출력
 cv2.imshow("origin", img)
 cv2.imshow("45", r45)
 cv2.imshow("90", r90)
+cv2.imshow("use function 45", img45)
+cv2.imshow("use function 90", img90)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
