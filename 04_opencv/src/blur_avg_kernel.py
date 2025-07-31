6222673a5585bf0979bc1eea85275b0fd970b0f3
Barkyeongchan <xodudcksxo@gmail.com>
2025-07-31 14:53:20 +0900

    함수를 사용해서 블러 적용

--- 04_opencv/src/blur_avg_kernel.py
+++ 04_opencv/src/blur_avg_kernel.py
@@ -4,23 +4,31 @@ import cv2
 import numpy as np
 
 img = cv2.imread('../img/paper.jpg')
-'''
-# @5x5 평균 필터 커널 생성
-kernel = np.array([[0.04, 0.04, 0.04, 0.04, 0.04],
-                   [0.04, 0.04, 0.04, 0.04, 0.04],
-                   [0.04, 0.04, 0.04, 0.04, 0.04],
-                   [0.04, 0.04, 0.04, 0.04, 0.04],
-                   [0.04, 0.04, 0.04, 0.04, 0.04]])
-'''
-# @5X5 평균 필터 커널 생성
-kernel = np.ones((5,5))/5**2
+#'''
+## @5x5 평균 필터 커널 생성
+#kernel = np.array([[0.04, 0.04, 0.04, 0.04, 0.04],
+#                   [0.04, 0.04, 0.04, 0.04, 0.04],
+#                   [0.04, 0.04, 0.04, 0.04, 0.04],
+#                   [0.04, 0.04, 0.04, 0.04, 0.04],
+#                   [0.04, 0.04, 0.04, 0.04, 0.04]])
+#'''
+## @5X5 평균 필터 커널 생성
+#kernel = np.ones((5,5))/5**2
+#
+## @필터 적용
+#blured = cv2.filter2D(img, -1, kernel)
+#
+## @이미지 출력
+#cv2.imshow('origin', img)
+#cv2.imshow('avrg blur', blured)
 
-# @필터 적용
-blured = cv2.filter2D(img, -1, kernel)
+# @blur() 함수로 블러링
+blur1 = cv2.blur(img, (10,10))
+# @boxFilter() 함수로 블러링 적용
+blur2 = cv2.boxFilter(img, -1, (10,10))
 
-# @이미지 출력
-cv2.imshow('origin', img)
-cv2.imshow('avrg blur', blured)
+merged = np.hstack( (img, blur1, blur2))
+cv2.imshow('blur', merged)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
