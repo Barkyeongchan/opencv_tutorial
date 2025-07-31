72c18659a487c007854d69f9253c532e23ce4f5d
Barkyeongchan <xodudcksxo@gmail.com>
2025-07-31 10:22:20 +0900

    함수로 확대, 축소 코드 추가

--- 04_opencv/src/scale_matrix.py
+++ 04_opencv/src/scale_matrix.py
@@ -23,12 +23,22 @@ dst3 = cv2.warpAffine(img, m_small, (int(height*0.5), int(width*0.5)), \
 dst4 = cv2.warpAffine(img, m_big, (int(height*2), int(width*2)), \
                         None, cv2.INTER_CUBIC)
 
+# @cv.2resize() 함수를 사용해 확대 축소
+# 크기 지정으로 축소
+func1 = cv2.resize(img, (int(width*0.5), int(height*0.5)), \
+                         interpolation=cv2.INTER_AREA)
+
+# 배율 지정으로 확대
+func2 = cv2.resize(img, None,  None, 2, 2, cv2.INTER_CUBIC)
+
 # @이미지 출력
 cv2.imshow("original", img)
 cv2.imshow("small", dst1)
 cv2.imshow("big", dst2)
 cv2.imshow("small INTER_AREA", dst3)
 cv2.imshow("big INTER_CUBIC", dst4)
+cv2.imshow("use Function small", func1)
+cv2.imshow("use Function big", func2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
