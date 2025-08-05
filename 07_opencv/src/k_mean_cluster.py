79f6c3cf636f2d6e3b114c6d0cf6264458ebe5bf
Barkyeongchan <xodudcksxo@gmail.com>
2025-08-05 10:50:59 +0900

    centroid의 위치 출력 추가

--- 07_opencv/src/k_mean_cluster.py
+++ 07_opencv/src/k_mean_cluster.py
@@ -6,5 +6,7 @@ from sklearn.cluster import KMeans
 X= -2 * np.random.rand(100,2)
 X1 = 1 + 2 * np.random.rand(50,2)
 X[50:100, :] = X1
-plt.scatter(X[ : , 0], X[ :, 1], s = 50, c = 'b')
+plt.scatter(X[ : , 0], X[ : , 1], s =50, c='b')
+plt.scatter(-0.94665068, -0.97138368, s=200, c='g', marker='s')
+plt.scatter(2.01559419, 2.02597093, s=200, c='r', marker='s')
 plt.show()
