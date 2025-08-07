b7535e643fe40ad78bd368b08f4e937928865450
Barkyeongchan <xodudcksxo@gmail.com>
2025-08-07 15:57:15 +0900

    완성

--- 09_opencv/src/face_cam_mosaic_park.py
+++ 09_opencv/src/face_cam_mosaic_park.py
@@ -6,8 +6,7 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
 
 cap = cv2.VideoCapture(0)
-#cap.set(cv2.cv2.CAP_PROP_FRAME_WIDTH, 480)
-#cap.set(cv2.cv2.CAP_PROP_FRAME_HEIGHT, 320)
+rate = 15  # 모자이크 비율
 
 while cap.isOpened():
     ret, img = cap.read()
@@ -17,32 +16,23 @@ while cap.isOpened():
     
     # 얼굴 영역 검출
     faces = detector(gray)
+
     for rect in faces:
 
         # 얼굴 영역을 좌표로 변환 후 사각형 표시
         x,y = rect.left(), rect.top()
         w,h = rect.right()-x, rect.bottom()-y
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
+
+        roi = img[y:y+h, x:x+w]
+        if roi.size == 0:
+            continue
+        roi = cv2.resize(roi, (w // rate, h // rate))
+        roi = cv2.resize(roi, (w, h), interpolation=cv2.INTER_AREA)
+        img[y:y+h, x:x+w] = roi
     
-    cv2.imshow("face landmark", img)
+    cv2.imshow("mosaic", img)
     if cv2.waitKey(1)== 27:
         break
 
-rate = 15               # 모자이크에 사용할 축소 비율 (1/rate)
-win_title = 'mosaic'    # 창 제목
-img = cv2.imread('../img/like_lenna.png')    # 이미지 읽기
-
-while True:
-    x,y,w,h = cv2.selectROI(win_title, img, False) # 관심영역 선택
-    if w and h:
-        roi = img[y:y+h, x:x+w]   # 관심영역 지정
-        roi = cv2.resize(roi, (w//rate, h//rate)) # 1/rate 비율로 축소
-        # 원래 크기로 확대
-        roi = cv2.resize(roi, (w,h), interpolation=cv2.INTER_AREA)  
-        img[y:y+h, x:x+w] = roi   # 원본 이미지에 적용
-        cv2.imshow(win_title, img)
-    else:
-        break
-    
 cv2.destroyAllWindows()
 cap.release()
