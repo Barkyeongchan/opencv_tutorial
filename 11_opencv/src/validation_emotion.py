092dc3919fcefc76ba89ab0bd79bce9d4af77550
Barkyeongchan <xodudcksxo@gmail.com>
2025-08-11 14:53:43 +0900

    이미지에서 얼굴만 검출

--- 11_opencv/src/validation_emotion.py
+++ 11_opencv/src/validation_emotion.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 
 image = cv2.imread('../data/img/charles.jpg')
 
-plt.figure(figsize=(8,8))
-plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-plt.show()
+# plt.figure(figsize=(8,8))
+# plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+# plt.show()
+
+face_detector = dlib.cnn_face_detection_model_v1('../data/weights/mmod_human_face_detector.dat')
+face_detection = face_detector(image, 1)
+
