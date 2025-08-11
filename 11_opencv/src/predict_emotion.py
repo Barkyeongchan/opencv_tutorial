6f7eca06a56137a93853c002820cd502f40fbb6c
Barkyeongchan <xodudcksxo@gmail.com>
2025-08-11 13:06:59 +0900

    모델 훈련과 모델 성능 평가 추가

--- 11_opencv/src/predict_emotion.py
+++ 11_opencv/src/predict_emotion.py
@@ -43,3 +43,69 @@ test_dataset = test_generator.flow_from_directory(directory='../data/test',
                                                   shuffle=False,
                                                   seed=10)
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, BatchNormalization
+from sklearn.metrics import accuracy_score
+
+num_classes = 7
+num_detectors = 32
+width, height = 48, 48
+
+network = Sequential()
+
+network.add(Conv2D(filters=num_detectors, kernel_size=3, activation='relu', padding='same', input_shape=(width, height, 3)))
+network.add(BatchNormalization())
+network.add(Conv2D(filters=num_detectors, kernel_size=3, activation='relu', padding='same'))
+network.add(BatchNormalization())
+network.add(MaxPooling2D(pool_size=(2, 2)))
+network.add(Dropout(0.2))
+
+network.add(Conv2D(2*num_detectors, 3, activation='relu', padding='same'))
+network.add(BatchNormalization())
+network.add(Conv2D(2*num_detectors, 3, activation='relu', padding='same'))
+network.add(BatchNormalization())
+network.add(MaxPooling2D(pool_size=(2, 2)))
+network.add(Dropout(0.2))
+
+network.add(Conv2D(2*2*num_detectors, 3, activation='relu', padding='same'))
+network.add(BatchNormalization())
+network.add(Conv2D(2*2*num_detectors, 3, activation='relu', padding='same'))
+network.add(BatchNormalization())
+network.add(MaxPooling2D(pool_size=(2, 2)))
+network.add(Dropout(0.2))
+
+network.add(Conv2D(2*2*2*num_detectors, 3, activation='relu', padding='same'))
+network.add(BatchNormalization())
+network.add(Conv2D(2*2*2*num_detectors, 3, activation='relu', padding='same'))
+network.add(BatchNormalization())
+network.add(MaxPooling2D(pool_size=(2, 2)))
+network.add(Dropout(0.2))
+
+network.add(Flatten())
+
+network.add(Dense(2*2*num_detectors, activation='relu'))
+network.add(BatchNormalization())
+network.add(Dropout(0.2))
+
+network.add(Dense(2*num_detectors, activation='relu'))
+network.add(BatchNormalization())
+network.add(Dropout(0.2))
+
+network.add(Dense(num_classes, activation='softmax'))
+
+network.summary()
+
+# 모델 훈련
+network.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+epochs = 5
+
+network.fit(train_dataset, epochs=epochs)
+
+# 모델 성능 평가
+network.evaluate(test_dataset)
+preds = network.predict(test_dataset)
+print(preds)
+preds = np.argmax(preds, axis=1)
+print(preds)
+print(test_dataset.classes)
+print(accuracy_score(test_dataset.classes, preds))
